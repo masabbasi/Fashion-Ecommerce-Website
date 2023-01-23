bb724d12bdd243fc4486f9c7538e7f7f5a0ae9d3
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-23 12:54:33 +0330

    End: Brand

--- js/app.js
+++ js/app.js
@@ -2,7 +2,39 @@ const $ = document;
 const hamburgerMenu = $.querySelector(".hamburgerMenu");
 const menuNav = $.querySelector(".menu nav");
 
-hamburgerMenu.addEventListener("click",()=>{
-	hamburgerMenu.classList.toggle("hamburgerMenuOpen");
-	menuNav.classList.toggle("navInMobile");
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("hamburgerMenuOpen");
+  menuNav.classList.toggle("navInMobile");
+});
+
+// swiper Bundle
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+		1400: {
+      slidesPerView: 4,
+    },
+  },
 });
